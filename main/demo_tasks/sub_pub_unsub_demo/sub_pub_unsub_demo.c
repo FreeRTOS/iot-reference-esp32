546d6dc854ed3099e7d6846d373d856bc5a037a6
Jason Carroll <czjaso@amazon.com>
2022-04-29 11:25:42 -0700

    Added copyright and versioning to files.

--- main/demo_tasks/sub_pub_unsub_demo/sub_pub_unsub_demo.c
+++ main/demo_tasks/sub_pub_unsub_demo/sub_pub_unsub_demo.c
@@ -1,7 +1,33 @@
 /*
- * This file demonstrates numerous tasks all of which use the MQTT agent API
+ * ESP32-C3 Featured FreeRTOS IoT Integration V202204.00
+ * Copyright (C) 2022 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * https://www.FreeRTOS.org
+ * https://github.com/FreeRTOS
+ *
+ */
+
+/*
+ * This file demonstrates numerous tasks all of which use the core-MQTT Agent API
  * to send unique MQTT payloads to unique topics over the same MQTT connection
- * to the same MQTT agent.
+ * to the same coreMQTT-Agent.
  *
  * Each created task is a unique instance of the task implemented by
  * prvSubscribePublishUnsubscribeTask().  prvSubscribePublishUnsubscribeTask()
@@ -27,6 +53,11 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
+
+/* ESP-IDF includes. */
+#include "esp_log.h"
+#include "esp_event.h"
+#include "sdkconfig.h"
 
 /* coreMQTT library include. */
 #include "core_mqtt.h"
